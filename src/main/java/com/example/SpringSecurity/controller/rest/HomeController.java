e344fa3dd070ca7279f479610c36ee7b7ee4e984
Dmitriy <dmitri1.z@yandex.ru>
2024-05-02 20:51:29 +0300

    second commit

--- src/main/java/com/example/SpringSecurity/controller/rest/HomeController.java
+++ src/main/java/com/example/SpringSecurity/controller/rest/HomeController.java
@@ -23,30 +23,55 @@ public class HomeController {
     public ResponseEntity<String> admin(){
         return new ResponseEntity<>(adminService.AdminFeature(),HttpStatus.I_AM_A_TEAPOT);
     }
-
-    @GetMapping("/user")
-    public ResponseEntity<List<Note>> user() {
+    @GetMapping("/admin/notes")
+    public ResponseEntity<List<Note>> AdminNotes() {
         return new ResponseEntity<>(userService.getAllNotes(), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Note> getById(@PathVariable Long id){
+    @GetMapping("/admin/{id}")
+    public ResponseEntity<Note> AdminGetById(@PathVariable Long id){
         return new ResponseEntity<>(userService.getById(id),HttpStatus.FOUND);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Note> createNewNote(@RequestBody Note note){
+    @PostMapping("/admin/create")
+    public ResponseEntity<Note> AdminCreateNewNote(@RequestBody Note note){
         return new ResponseEntity<>(userService.createNote(note), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Note> deleteNote(@PathVariable Long id){
+    @DeleteMapping("/admin/delete/{id}")
+    public ResponseEntity<Note> AdminDeleteNote(@PathVariable Long id){
         userService.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/edit/{id}")
-    public ResponseEntity<Note> editNote(@PathVariable Long id, @RequestBody Note note){
+    @PutMapping("/admin/edit/{id}")
+    public ResponseEntity<Note> AdminEditNote(@PathVariable Long id, @RequestBody Note note){
+        return new ResponseEntity<>(userService.editNote(id,note), HttpStatus.I_AM_A_TEAPOT);
+    }
+
+    @GetMapping("/user/notes")
+    public ResponseEntity<List<Note>> UserNotes() {
+        return new ResponseEntity<>(userService.getAllNotes(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Note> UserGetById(@PathVariable Long id){
+        return new ResponseEntity<>(userService.getById(id),HttpStatus.FOUND);
+    }
+
+    @PostMapping("/user/create")
+    public ResponseEntity<Note> UserCreateNewNote(@RequestBody Note note){
+        return new ResponseEntity<>(userService.createNote(note), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/user/delete/{id}")
+    public ResponseEntity<Note> UserDeleteNote(@PathVariable Long id){
+        userService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/user/edit/{id}")
+    public ResponseEntity<Note> UserEditNote(@PathVariable Long id, @RequestBody Note note){
         return new ResponseEntity<>(userService.editNote(id,note), HttpStatus.I_AM_A_TEAPOT);
     }
 
